798cdf874efa3429cdf8104effcea031c0efa84e
nnamdi16 <nwabuokeinnamdi@gmail.com>
2023-02-13 12:15:58 +0100

    chore: moved files to the right folder

--- src/config/keys.ts
+++
@@ -1,3 +0,0 @@
-export default {
-    mongoURI: "mongodb+srv://tochukwu1159:tochukwu1159@cluster1.lwp8cvl.mongodb.net/?retryWrites=true&w=majority"
-}
--- src/qrcode/qr.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { QrcodeController } from './qrcode.controller';
-
-@Module({
-  controllers: [QrcodeController],
-})
-export class QrModule {}
